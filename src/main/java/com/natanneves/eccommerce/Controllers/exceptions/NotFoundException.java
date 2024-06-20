2617e2d641e45c95441c515f812ee6d22a261509
Natan Neves <dev.natanbarbosa@gmail.com>
2024-06-20 17:48:33 -0300

    final commit

--- src/main/java/com/natanneves/eccommerce/Controllers/exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.natanneves.eccommerce.Controllers.exceptions;
-
-public class NotFoundException extends RuntimeException{
-
-    public NotFoundException(String msg){
-        super(msg);
-    }
-}
--- src/main/java/com/natanneves/eccommerce/repositories/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.natanneves.eccommerce.repositories;
-
-import com.natanneves.eccommerce.domain.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
